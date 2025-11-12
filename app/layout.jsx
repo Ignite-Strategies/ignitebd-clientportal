8ebb6d5a44c14fb8aae62e611323e3c064c25203
Adam Cole <adam@gofastcrushgoals.com>
2025-11-12 13:33:05 -0500

    Revert to favicon.png - restore correct favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,9 +4,9 @@ export const metadata = {
   title: 'Ignite Client Portal',
   description: 'Your engagement hub for proposals and deliverables',
   icons: {
-    icon: '/favicon.svg',
-    shortcut: '/favicon.svg',
-    apple: '/favicon.svg',
+    icon: '/favicon.png',
+    shortcut: '/favicon.png',
+    apple: '/favicon.png',
   },
 };
 
